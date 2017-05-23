b8b02bbd774dd5216ccc4542dcdb3b090970b5a8
Student User <student@cti.pub.ro>
2017-05-23 11:57:36 +0300

    final

--- PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/network/CommunicationThread.java
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/network/CommunicationThread.java
@@ -60,29 +60,78 @@ public class CommunicationThread extends Thread {
                 Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error receiving parameters from client (city / information type!");
                 return;
             }
-            HashMap<String, TimeInformation> data = serverThread.getData();
-            TimeInformation timeInformation = null;
-            /*
-            if (data.containsKey(city)) {
-                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the cache...");
-                timeInformation = data.get(city);
-            } else {
-                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the webservice...");
 
-                String hours = "10";
-                String minutes = "02";
-                String seconds = "05";
-                timeInformation = new TimeInformation(
-                        hours, minutes, seconds
-                );
-                }
-            }*/
-            if (timeInformation == null) {
-                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Weather Forecast Information is null!");
+            String[] timeArray = time.split(",");
+
+            if (clientOp.equals("SET")) {
+                TimeInformation newAlarm = new TimeInformation(timeArray[0], timeArray[1], timeArray[2]);
+                serverThread.setData(time, newAlarm);
+
+                printWriter.println(time);
+                printWriter.flush();
+                return;
             }
-            String result = clientOp;
-            printWriter.println(result);
-            printWriter.flush();
+
+            if (clientOp.equals("RESET")) {
+                HashMap<String, TimeInformation> data = serverThread.getData();
+                if (data.containsKey(time)) {
+                    data.remove(time);
+                    printWriter.println("REMOVED " + time);
+                    printWriter.flush();
+                }
+
+                else {
+                    printWriter.println("DID NOT FOUND ALARM " + time);
+                    printWriter.flush();
+                }
+
+                return;
+            }
+
+            if (clientOp.equals("POLL")) {
+                HashMap<String, TimeInformation> data = serverThread.getData();
+                if (data.containsKey(time)) {
+                    Socket socket = new Socket("utcnist.colorado.edu", 13);
+                    if (socket == null) {
+                        Log.e(Constants.TAG, "[COMMUNICATION THREAD] Could not create socket!");
+                        return;
+                    }
+                    BufferedReader bufferedReaderS = Utilities.getReader(socket);
+                    bufferedReaderS.readLine();
+                    String line = bufferedReaderS.readLine();
+                    String[] timeData = line.split(" ");
+                    String[] timeArray2 = timeData[2].split(":");
+
+                    Log.i(Constants.TAG, timeData[2]);
+
+                    TimeInformation alarm = data.get(time);
+                    if (Integer.parseInt(alarm.getHours()) > Integer.parseInt(timeArray2[0])) {
+                        printWriter.println("NOT EXPIRED");
+                    }
+
+                    else if (Integer.parseInt(alarm.getMinutes()) > Integer.parseInt(timeArray2[1])) {
+                        printWriter.println("NOT EXPIRED");
+                    }
+
+                    else if (Integer.parseInt(alarm.getSeconds()) > Integer.parseInt(timeArray2[2])) {
+                        printWriter.println("NOT EXPIRED");
+                    }
+
+                    else {
+                        printWriter.println("EXPIRED");
+                    }
+
+                    printWriter.flush();
+                    socket.close();
+                }
+
+                else {
+                    printWriter.println("DID NOT FOUND ALARM " + time);
+                    printWriter.flush();
+                }
+
+                return;
+            }
         } catch (IOException ioException) {
             Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
             if (Constants.DEBUG) {
